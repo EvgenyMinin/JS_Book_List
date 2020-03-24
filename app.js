11e682e47826e13beabb085f25958324b1f89361
Johnny1992 <evgeny.frontend@gmail.com>
2020-03-24 22:00:36 +0300

    added delete book from list

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 };
 
+UI.prototype.deleteBook = function(target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 UI.prototype.clearFields = function() {
   document.getElementById("title").value = "";
   document.getElementById("author").value = "";
@@ -56,4 +62,11 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
   }
 
   e.preventDefault();
+});
+
+document.getElementById("book-list").addEventListener("click", function(e) {
+  e.preventDefault();
+  const ui = new UI();
+  ui.deleteBook(e.target);
+  ui.showAlert("Book removed!", "success");
 });
